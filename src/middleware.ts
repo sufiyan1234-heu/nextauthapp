1870ffe4349b4caac8085c7c8b6413d378bc9cd8
Sufiyan <sufiyanchishty71@gmail.com>
2024-04-01 11:44:33 +0500

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname; //to get current path
-
+  console.log(path);
   const isPublic =
     path === "/login" || path === "/signup" || path === "/verifyemail";
 
